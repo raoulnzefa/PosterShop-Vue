b0297f6f0bb6dc694a568904313d504d47d36c42
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-22 17:50:07 -0400

    Code Refactor

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="header">
+      <h1>Poster Shop</h1>
+    </div>
+    <div class="main">
+      <div class="products">
+        <p>Products go here.</p>
+      </div>
+      <div class="cart">
+        <h2>Shopping Cart</h2>
+        <div>
+          No items in the cart.
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
