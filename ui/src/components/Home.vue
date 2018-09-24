77700dfafaa750e28448313475e4aab57d6c6137
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-23 21:28:42 -0400

    Add action to add items to a cart

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -5,8 +5,10 @@
     </div>
     <div class="main">
       <div class="products">
-        <div v-for="item in items" v-bind:key="item.id">{{item.title}}</div>
-        <button v-on:click="addItem">Add Item</button>
+        <div class="product" v-for="(item, index) in items" v-bind:key="item.id">
+          <h4 class="product-title">{{item.title}}</h4>
+          <button class="add-to-cart" v-on:click="addItem(index)">Add to cart</button>
+        </div>
       </div>
       <div class="cart">
         <h2>Shopping Cart</h2>
@@ -28,12 +30,13 @@ export default {
       { id: 2, title: 'Item 2' },
       { id: 3, title: 'Item 3' },
       { id: 4, title: 'Item 4' }
-    ]
+    ],
+    cart: []
   }),
   methods: {
-    addItem() {
+    addItem(index) {
+      console.log(index)
       this.total += 9.99
-      console.log(this.total)
     }
   }
 }
