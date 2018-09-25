81ca6ec72be0335398d024100e8c920c8f2ed0e6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-24 22:21:59 -0400

    Use vue-resource package

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueResource from 'vue-resource'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.http.options.emulateJSON = true
 
 new Vue({
   router,
