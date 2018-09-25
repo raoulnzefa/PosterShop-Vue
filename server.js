11657e7842da73fff51185438f9d6c2430632f9f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-24 22:22:18 -0400

    Add prefix to api endpoint

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var axios = require('axios');
-var querystring = require('querystring');
+var express = require('express')
+var app = express()
+var path = require('path')
+var server = require('http').createServer(app)
+var axios = require('axios')
+var querystring = require('querystring')
 
-require('dotenv').config();
+require('dotenv').config()
 
-var bodyParser = require('body-parser');
-app.use( bodyParser.json() );
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 var instance = axios.create({
   baseURL: 'https://api.imgur.com/3/',
-  headers: { 'Authorization': 'Client-ID ' + process.env.IMGUR_CLIENT_ID }
-});
+  headers: { Authorization: 'Client-ID ' + process.env.IMGUR_CLIENT_ID }
+})
 
-app.get('/search/:query', function(req, res) {
-  const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query });
-  instance.get(url)
-    .then(function (result) {
-      res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated));
+app.get('/api/search/:query', function(req, res) {
+  const url =
+    'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query })
+  instance
+    .get(url)
+    .then(function(result) {
+      res.send(
+        result.data.data.filter(
+          item => !item.is_album && !item.nsfw && !item.animated
+        )
+      )
     })
-    .catch(function (error) {
-      console.log(error);
+    .catch(function(error) {
+      console.log(error)
     })
-  ;
-});
+})
 
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 if (process.env.NODE_ENV !== 'production') {
-  require('reload')(server, app);
+  require('reload')(server, app)
 }
 
-server.listen(process.env.PORT, function () {
+server.listen(process.env.PORT, function() {
   console.log('Listening on port '.concat(process.env.PORT))
-});
+})
